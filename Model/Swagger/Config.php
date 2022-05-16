668caef9e1031f13fa437b0e20bae8de34e1fb2f
Mickaël BAILLOT <mickael.baillot@ph2m.com>
2022-05-16 09:57:29 +0200

    Create PH2M Api module

---
+++ Model/Swagger/Config.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+/**
+* Copyright © PH2M SARL. All rights reserved.
+* See COPYING.txt for license details.
+*/
+namespace PH2MCommonApi\Model\Swagger;
+
+use Magento\Framework\App\State;
+
+class Config extends \Magento\Swagger\Model\Config
+{
+    /**
+     * @param State $state
+     * @param bool $enabledInProduction
+     */
+    public function __construct(State $state, bool $enabledInProduction = true)
+    {
+        parent::__construct($state, $enabledInProduction);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# PH2MApi
+PH2M_CommonApi
+-------
+PH2M Common api override.
+
+Requirements
+------------
+Magento >= 2.3.7
+
+Installation
+------------
+```
+composer require ph2m/common-api
+bin/magento module:enable PH2M_CommonApi
+bin/magento setup:upgrade
+```
